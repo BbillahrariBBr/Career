04c5c76949019218bee3cc2254b105221defd83f
Bakibillah <mb.billah8@gmail.com>
2019-10-22 01:17:01 +0600

    Country activity design finished

--- app/src/main/java/com/example/career/SpecialActivity.java
+++ app/src/main/java/com/example/career/SpecialActivity.java
@@ -2,13 +2,78 @@ package com.example.career;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SpecialActivity extends AppCompatActivity {
+    ImageView sub_review, cal,video, trends,tips, transport ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special);
+        sub_review = (ImageView) findViewById(R.id.sub_review);
+        cal = (ImageView) findViewById(R.id.calender);
+        video = (ImageView) findViewById(R.id.video);
+        trends = (ImageView) findViewById(R.id.trends);
+        tips = (ImageView) findViewById(R.id.tips);
+        transport = (ImageView) findViewById(R.id.transport);
+
+        sub_review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
+
+        cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
+
+        video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
+
+        trends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
+
+        tips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
+
+        transport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),NoticeActivity.class);
+                startActivity(intent);
+                //working
+            }
+        });
     }
 }
